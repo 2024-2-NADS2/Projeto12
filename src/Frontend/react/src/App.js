c4f25937d1b90e95ca9435178eabbc488052a030
Anderson Fernandez <103613700+anderson-fndz@users.noreply.github.com>
2024-10-20 21:28:57 +0000

    adicionando react

---
+++ src/Frontend/react/src/App.js
@@ -0,0 +1,15 @@
+import Form from "./Components/form/Form";
+import Header from "./Components/Header/header"
+import Button from "./Components/Button/button";
+import Cadastro from "./pages/Cadastro/cadastro";
+import Home from "./pages/Home/home";
+
+function App() {
+  return (
+    <body>
+      <Home />
+    </body>
+  );
+}
+
+export default App;
---
+++ src/Frontend/react/src/pages/Home/home.js
@@ -0,0 +1,15 @@
+import styles from './home.css'
+import Header from '../../Components/Header/header'
+
+function Home() {
+    return (
+        <div>
+            <Header></Header>
+            <div className="section">
+
+            </div>
+        </div>
+    )
+}
+
+export default Home
